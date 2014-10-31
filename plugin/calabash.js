0c501d47731665080c30e226c2c7a6b4aa7ffb83
Terry Morgan <terry.morgan76@gmail.com>
2014-10-31 11:25:56 +0000

    Sheepishly reverting logger.exception back to throw for exceptions

--- plugin/calabash.js
+++ plugin/calabash.js
@@ -55,7 +55,7 @@ exports.run = function(logger, config, cli, finished) {
 
     /* if they are not using ios or android, this command should gracefully bow out*/
     if (['android', 'ios', 'iphone'].indexOf(platform) === -1) {
-        logger.exception('Calabash does not support your build target. \n Mobile Web support is planned, but not supported at this time.');
+        throw 'Calabash does not support your build target. \n Mobile Web support is planned, but not supported at this time.';
     }
 
     if (fs.existsSync(path.join(projectDir, 'tiapp.xml'))) {
